907362a759ca01f101c10efa3a05aa70871a37c5
github_username <email_address>
2025-02-28 20:40:03 +0530

    single agents demo

--- Agentic-AI/CrewAI-Agents-Demo/single-agents-demo.py
+++ Agentic-AI/CrewAI-Agents-Demo/single-agents-demo.py
@@ -2,37 +2,44 @@ import os
 from dotenv import load_dotenv
 from crewai import Agent, Task, Crew
 from crewai_tools import SerperDevTool
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 
 load_dotenv()
 
 SERPER_API_KEY = os.getenv("SERPER_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-search_tool = SERPER_API_KEY()
-llm = ChatOpenAI(model="gpt-3.5-turbo")
+search_tool = SerperDevTool()
+
 
 def create_research_agent():
+
+    llm = ChatGroq(
+        temperature=0,
+        model="llama3-70b-8192",
+        api_key=GROQ_API_KEY)
+
     return Agent(
             role = "Research Specialist",
-            goal = "Conduct through research on topics",
-            backstory = "You are an experienced researcher with expertise in finding and synthesizing information from variance source",
+            goal = "Conduct through research on given topics",
+            backstory = "You are an experienced researcher with expertise in finding and synthesizing information from various sources",
             verbose = True,
             allow_delegation = False,
             tools = [search_tool],
             llm = llm
     )
 
-def create_research_task(agent, task):
+def create_research_task(agent, topic):
     return Task(
-            description = f"Research the following topic and provide a commprehensive summary: {topic}",
+            description = f"Research the following topic and provide a comprehensive summary: {topic}",
             agent = agent,
-            excepted_output = "A detailed summary of the research findings, including key points and insights related to an topics"
+            expected_output = "A detailed summary of the research findings, including key points and insights related to the topic"
     )
 
 def run_research(topic):
     agent = create_research_agent()
-    task = create_research_task()
-    crew = crew(agent = [agent], task =[task])
+    task = create_research_task(agent, topic)
+    crew = Crew(agents = [agent], tasks =[task])
     results = crew.kickoff()
     return results
 
